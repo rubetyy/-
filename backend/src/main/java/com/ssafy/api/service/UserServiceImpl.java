539505e5f1c8b517f2e23ae9f5f44a5d92637697
김혜원 <akrnrh9811@gmail.com>
2021-07-20 22:12:51 +0900

    feat:로그인 예외 처리 추가

--- backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -45,7 +45,7 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUserByUserId(String userId) {
 		// 디비에 유저 정보 조회 (userId 를 통한 조회).
-		User user = userRepositorySupport.findUserByUserId(userId).get();
+		User user = userRepositorySupport.findUserByUserId(userId).orElse(null);
 		return user;
 	}
 
