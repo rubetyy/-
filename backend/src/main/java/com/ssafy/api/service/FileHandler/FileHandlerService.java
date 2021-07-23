40b7755a046c9c6df66d77bd6872d9a82cdbe590
김혜원 <akrnrh9811@gmail.com>
2021-07-23 13:47:08 +0900

    S05P12C103-73 feat : [BE] 파일 업로드 서비스 구현, 상품 등록 구현 완료

--- backend/src/main/java/com/ssafy/api/service/FileHandler/FileHandlerService.java
+++ backend/src/main/java/com/ssafy/api/service/FileHandler/FileHandlerService.java
@@ -1,11 +1,14 @@
 package com.ssafy.api.service.FileHandler;
 
 import com.ssafy.api.request.ImageRegister;
+import com.ssafy.db.entity.Image;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FileHandlerService {
-    public void upload(MultipartFile file);
+    public List<Image> upload(List<MultipartFile> file);
     public void download(String filename);
     public void deleteFile();
 }
