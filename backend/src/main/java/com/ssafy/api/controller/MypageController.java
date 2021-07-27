60a4edc19f7f51fb917e90df80e501c3a30fd113
최성은 <yosbes21@naver.com>
2021-07-27 16:20:02 +0900

    final_

---
+++ backend/src/main/java/com/ssafy/api/controller/MypageController.java
@@ -0,0 +1,12 @@
+package com.ssafy.api.controller;
+
+import com.ssafy.common.model.response.BaseResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MypageController {
+    public ResponseEntity<? extends BaseResponseBody> mypage(){
+        return null;
+    }
+}
