ce96b400802e406f5acc5d128d7af7d041194ed8
rubetyy <nightyong25@naver.com>
2021-08-05 15:14:10 +0900

    feat: [FE] MainPage 이미지 나오게

--- frontend/src/components/MainPage/ProductListItem.vue
+++ frontend/src/components/MainPage/ProductListItem.vue
@@ -2,8 +2,9 @@
   <div class="col">
     <!-- 상품 이미지랑 제목만 보내는 걸로 -->
     <div class="card">
+      {{this.product.filePath}}
       <router-link :to="`/product/${product.product.id}`">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3eBNF4vrGY7wZXQWf38XttL5wDckdUG-M4A&usqp=CAU" class="card-img-top" alt="...">
+        <img :src="`http://i5c103.p.ssafy.io/${image}`" class="card-img-top" alt="...">
         <!-- <img :src="product.thumbnail" class="image"> ↑임시주소 -->
         <div class="card-body">
           <h5>{{ product.product.title }}</h5>
@@ -21,9 +22,14 @@ export default {
   },
   data: function() {
     return {
-
+      
     }
   },
+  computed: {
+    image() {
+      return this.product.filePath
+    }
+  }
 
 }
 </script>
