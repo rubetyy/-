0b20e90f668cf213373317d525faff96fa410265
최성은 <yosbes21@naver.com>
2021-08-09 15:37:31 +0900

    S05P13C103-55 [BE] :feat config파일 /sub, /pub prefix수정

--- backend/src/main/java/com/ssafy/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/config/WebSocketConfig.java
@@ -19,9 +19,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("pub");
+//        registry.setApplicationDestinationPrefixes("/app");
+//        registry.enableSimpleBroker("/topic");
 //        registry.enableSimpleBroker("/send");
     }
 }
