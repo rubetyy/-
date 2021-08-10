3bf52bee3f4033bc4c81648e0dd96e7143c20585
rubetyy <nightyong25@naver.com>
2021-08-10 17:48:20 +0900

    feat: [FE] 메인 liveList

--- frontend/src/components/CarouselList.vue
+++ frontend/src/components/CarouselList.vue
@@ -1,42 +1,57 @@
 
 <template>
-<swiper :slidesPerView="4" :spaceBetween="30" :pagination='{
-  "clickable": true
-}' class="mySwiper">
-  <swiper-slide>Slide 1</swiper-slide>
-  <swiper-slide>Slide 2</swiper-slide>
-  <swiper-slide>Slide 3</swiper-slide>
-  <swiper-slide>Slide 4</swiper-slide>
-  <swiper-slide>Slide 5</swiper-slide>
-  <swiper-slide>Slide 6</swiper-slide>
-  <swiper-slide>Slide 7</swiper-slide>
-  <swiper-slide>Slide 8</swiper-slide>
-  <swiper-slide>Slide 9</swiper-slide>
-  </swiper>
+<!-- Slider main container -->
+<div class="swiper-container">
+  <!-- Additional required wrapper -->
+  <!-- {{lives[0]}} -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    
+    <div class="swiper-slide" v-for="(live, idx) in lives" :key="idx" :live="live"> 
+      <img :src="`http://i5c103.p.ssafy.io:8080/${live.filepath}`"  alt="" style="width:100%; height:90%;">
+      <p>{{live.livetitle}}
+      <router-link :to="{name: 'LivePage', params: { id: live.livepk }}">라이브 방송 시청</router-link>
+      </p>
+
+    </div>
+    <!-- <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    <div class="swiper-slide">Slide 4</div>
+    <div class="swiper-slide">Slide 5</div>
+    <div class="swiper-slide">Slide 6</div>
+    <div class="swiper-slide">Slide 7</div>
+    <div class="swiper-slide">Slide 8</div>
+    <div class="swiper-slide">Slide 9</div> -->
+  ...
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
 </template>
 <script>
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+  // import Swiper from 'swiper';
+  // import Swiper styles
+  import 'swiper/swiper-bundle.css';
+  // import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+  import Swiper from 'swiper/bundle';
 
-// Import Swiper styles
-import 'swiper/swiper.scss';
-import "swiper/components/pagination/pagination.min.css"
-import './style.css';
-
-
-// import Swiper core and required modules
-import SwiperCore, {
-  Pagination
-} from 'swiper/core';
-
-// install Swiper modules
-SwiperCore.use([Pagination]);
-
+  // configure Swiper to use modules
+  // SwiperCore.use([Navigation, Pagination]);
 
 export default {
-  components: {
-    Swiper,
-    SwiperSlide,
+  // components: {
+  //   Swiper,
+    
+  // },
+  props: {
+    lives: Array,
   },
   data() {
     return {
@@ -44,9 +59,61 @@ export default {
         
     };
   },
+  mounted() {
+    const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 50,
+    // breakpoints: {
+    //   1024: {
+    //     slidesPerView: 4,
+    //     spaceBetween: 40
+    //   },
+    //   768: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30
+    //   },
+    //   640: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20
+    //   },
+    //   320: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 10
+    //   }
+    // },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    
+    });
+    // const swiper = document.querySelector('.swiper-container').swiper;
+    swiper.slideNext();
+
+  },
   methods: {
     
   }
   
 }
 </script>
+<style scoped>
+.swiper-container {
+  width: 1280px;
+  height: 700px;
+}
+</style>
