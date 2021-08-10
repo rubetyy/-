b1903aae3c3a5603988ec66b7c3bd5374d961b40
이연지 <yeonji1012@naver.com>
2021-08-10 15:05:31 +0900

    S05P13C103-56 feat: [BE] 실시간채팅 roomId 구분 완료

--- backend/src/main/java/com/ssafy/api/controller/ChatController.java
+++ backend/src/main/java/com/ssafy/api/controller/ChatController.java
@@ -15,11 +15,10 @@ public class ChatController {
     public void message(ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType()))
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        System.out.println("연지연지");
-        System.out.println("연지연지");
-        System.out.println("연지연지");
-        System.out.println("연지연지");
-        System.out.println("연지연지");
+        System.out.println("yeonji");
+        System.out.println("yeonji");
+        System.out.println("yeonji");
+        System.out.println("yeonji");
         System.out.println("연지연지");
         System.out.println("연지연지");
         System.out.println("연지연지");
