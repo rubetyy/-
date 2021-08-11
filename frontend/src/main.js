da3a9d1562f8ab80771428190df34552878dc868
cgkong <cgkong@naver.com>
2021-08-11 17:49:08 +0900

    부트스트랩 수정 완료

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from '@/store/index.js'
+
 // 부트스트랩
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(BootstrapVue)
-
 
 Vue.config.productionTip = false
 
