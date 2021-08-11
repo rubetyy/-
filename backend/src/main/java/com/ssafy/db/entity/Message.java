68956632ec199e701538e08df60b5b87538fe080
최성은 <yosbes21@naver.com>
2021-08-11 18:06:11 +0900

    테이블 칼럼 오류 해결

--- backend/src/main/java/com/ssafy/db/entity/Message.java
+++ backend/src/main/java/com/ssafy/db/entity/Message.java
@@ -16,23 +16,22 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@ToString
 @Table(name="chat_message")
 public class Message {
     @Id
-    @Column("chat_message_pk")
+    @Column(name = "chat_message_pk")
     Integer id;
 
-    @Column("chat_room_pk")
+    @Column(name = "chat_room_pk")
     Integer roomId;
 
-    @Column("user_id_sender")
+    @Column(name = "user_id_sender")
     String sender;
 
-    @Column("chat_message_content")
+    @Column(name = "chat_message_content")
     String content;
 
-    @Column("chat_message_created_at")
+    @Column(name = "chat_message_created_at")
     LocalDateTime now;
 
     public Message(ChatMessage message){
