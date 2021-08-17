89e92001d4b92dbc3414f17e61b0948a3a0e109f
Ubuntu <ubuntu@ip-172-26-6-85.ap-northeast-2.compute.internal>
2021-08-17 15:39:46 +0000

    이미지 로드 및 경로 수정

--- backend/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -35,7 +35,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         // 디랙토리 추가
-        registry.addResourceHandler("/api/images/**")
+        registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:////home/ubuntu/Spring/S05P13C103/backend/images/");
 
     		registry.addResourceHandler("/resources/**")
