ddff662b22692ec09769880041dfb0e9fbaadf01
cgkong <cgkong@naver.com>
2021-08-17 10:59:07 +0900

    [FE] 마이페이지 디자인 수정

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -5,39 +5,39 @@
       <h3>ID: {{myPageInfo.userinfo.userid}}</h3>
     </div>
 
-    <div class='d-flex justify-content-evenly'>
+    <div class='d-flex justify-content-evenly container'>
       <ul>
         <h2>판매중</h2>
         <li v-for='product in myPageInfo.issold' :key="product.id">
-          <router-link :to="{name: 'ProductDetail', params: {product_pk: product.id}}">{{product.title}}</router-link>
+          <router-link :to="{name: 'ProductDetail', params: {product_pk: product.id}}">-{{product.title}}</router-link>
         </li>
       </ul>
       <ul>
         <h2>판매완료</h2>
         <li v-for='soldout in myPageInfo.soldout' :key="soldout.id">
-          <router-link :to="{name: 'ProductDetail', params: {product_pk: soldout.id}}">{{soldout.title}}</router-link>
+          <router-link :to="{name: 'ProductDetail', params: {product_pk: soldout.id}}">-{{soldout.title}}</router-link>
         </li>
       </ul>
       <ul>
         <h2>구매완료</h2>
         <li v-for='orderProduct in myPageInfo.orderlist' :key="orderProduct.productpk">
-          <router-link :to="{name: 'ProductDetail', params: {product_pk: orderProduct.id}}">{{orderProduct.title}}</router-link>
+          <router-link :to="{name: 'ProductDetail', params: {product_pk: orderProduct.id}}">-{{orderProduct.title}}</router-link>
         </li>
       </ul>
       <ul>
         <h2>찜</h2>
         <li v-for='wishProduct in myPageInfo.wishlist' :key="wishProduct.id">
-          <router-link :to="{name: 'ProductDetail', params: {product_pk: wishProduct.id}}">{{wishProduct.title}}</router-link>
+          <router-link :to="{name: 'ProductDetail', params: {product_pk: wishProduct.id}}">-{{wishProduct.title}}</router-link>
         </li>
       </ul>
       <ul>
         <h2>채팅</h2>
           <li v-for='chatroom in myPageInfo.chatlist' :key="chatroom.chatroompk">
             <span v-if='userId==chatroom.useridseller'>
-              <router-link :to="{name: 'ChatRoom', params: {pk: chatroom.chatroompk}, query: {nickname: chatroom.buyernickname, productpk: chatroom.productpk  }}">{{chatroom.buyernickname}}</router-link>
+              <router-link :to="{name: 'ChatRoom', params: {pk: chatroom.chatroompk}, query: {nickname: chatroom.buyernickname, productpk: chatroom.productpk  }}">-{{chatroom.buyernickname}}</router-link>
             </span>
             <span v-else>
-              <router-link :to="{name: 'ChatRoom', params: {pk: chatroom.chatroompk}, query: {nickname: chatroom.sellernickname, productpk: chatroom.productpk }}">{{chatroom.sellernickname}}</router-link>
+              <router-link :to="{name: 'ChatRoom', params: {pk: chatroom.chatroompk}, query: {nickname: chatroom.sellernickname, productpk: chatroom.productpk }}">-{{chatroom.sellernickname}}</router-link>
             </span>
           </li>
       </ul>
@@ -83,5 +83,9 @@ export default {
   background-color: #fff3eb;
   margin: 40px auto 50px;
   padding: 30px 50px;
+}
+
+ul {
+  width: 300px;
 }
 </style>
